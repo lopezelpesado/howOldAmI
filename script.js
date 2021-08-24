c7b39780a00df43b9a916c2980bb5919e641dd67
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-08-24 19:07:59 +0100

    calculate time since js
    closes #3

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const form = document.getElementById("formBirthday");
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	form.howOld.textContent = form.birthday.value;
+	form.howOld.textContent = timeSince(form.birthday.value);
 });
+
+function timeSince(startDate) {
+	let now = Date.now();
+	console.log(now);
+
+	let then = Date.parse(startDate);
+	console.log(then);
+
+	millisecondsSince = now - then;
+	console.log(millisecondsSince);
+
+	return millisecondsSince / 1000 / 60 / 60 / 24 / 365;
+}
 
 console.log(form.elements);
